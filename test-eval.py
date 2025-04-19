238d5fb06ae6476cce1558c8ba84c185b7db87c3
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2025-04-19 11:07:11 +0000

    feat: test-eval func

---
+++ test-eval.py
@@ -0,0 +1,59 @@
+import json
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.meteor_score import meteor_score
+from sklearn.metrics import accuracy_score
+import numpy as np
+import nltk
+import sys
+
+nltk.download('wordnet') # run this once
+
+with open("/home/lenovo/exp1/data/openvivqa/vqa/openvivqa_test_v2.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# print(data[0])
+
+gt_dict = {item["id"]: item["answers"][0] for item in data["annotations"]}
+
+
+with open("/home/lenovo/exp1/output-dir/submit_openvivqa_test.json", "r", encoding="utf-8") as f:
+    predictions = json.load(f)
+
+# print(predictions[0])
+pred_dict = {item["question_id"]: item["answer"] for item in predictions}
+
+print(f"GT: {len(gt_dict)} items")
+print(f"Pred: {len(pred_dict)} items")
+
+assert len(gt_dict) == len(pred_dict)
+
+# qid, pred = pred_dict.items().__iter__().__next__()
+# gt = gt_dict.get(qid)
+# print(f"GT: {gt}")
+# print(f"Pred: {pred}")
+
+# sys.exit(0)
+
+smoothie = SmoothingFunction().method4
+
+bleu_scores = []
+meteor_scores = []
+exact_match = []
+
+for qid, pred in pred_dict.items():
+    gt = gt_dict.get(qid)
+    if gt:
+        gt_tokens = gt.split()
+        pred_tokens = pred.split()
+
+        bleu = sentence_bleu([gt_tokens], pred_tokens, smoothing_function=smoothie)
+        meteor = meteor_score([gt_tokens], pred_tokens)
+        exact = int(gt.strip().lower() == pred.strip().lower())
+
+        bleu_scores.append(bleu)
+        meteor_scores.append(meteor)
+        exact_match.append(exact)
+
+print(f"BLEU Score: {np.mean(bleu_scores):.4f}")
+print(f"METEOR Score: {np.mean(meteor_scores):.4f}")
+print(f"Exact Match Accuracy: {np.mean(exact_match):.4f}")
